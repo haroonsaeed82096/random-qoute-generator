8f7962b8a364f1665fc368942cc1affb57d1c954
unknown <haroon.saeed@outlook.com>
2020-11-01 18:49:30 +0500

    Add Loader and Finalize Project

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ const quoteText = document.getElementById('qoute');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-qoute');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+
+}
+
 // Show New Quotes
 function newQuote(){
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     if (!quote.author) {
         authorText.textContent = 'Unknown';
@@ -23,7 +38,7 @@ function newQuote(){
     }
 
     quoteText.textContent = quote.text;
-
+    complete();
 }
 
 // Tweet Quote
@@ -34,6 +49,7 @@ function tweetQuote(){
 
 // Get Qoute From API
 async function getQoute() {
+    loading();
     const apiURL = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiURL);
